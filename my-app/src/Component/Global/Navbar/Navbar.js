7bd266bf7fb3bb44d31b0fe57b797a341fdeed63
hetvigorasiya <gorasiyahetvi@gmail.com>
2024-05-09 10:33:17 +0530

    add

--- my-app/src/Component/Global/Navbar/Navbar.js
+++ my-app/src/Component/Global/Navbar/Navbar.js
@@ -6,47 +6,74 @@ import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
     return (
-        <div className="bg-light" style={{height:'80px'}}>
-            <nav className="container navbar navbar-expand-lg custom_nav-container pt-3">
+        // <div className="bg-light" style={{height:'80px'}}>
+        //     <nav className="container navbar navbar-expand-lg custom_nav-container pt-3">
+
+        //         <div className="navbar-brand">
+        //             <img width="200px" src={imgConfig?.logo1} alt="#" className='logo' />
+        //         </div>
+
+        //         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        //             <span class="navbar-toggler-icon"></span>
+        //         </button>
+
+        //         <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+
+        //             <ul className="navbar-nav fs-5 fw-bold">
+        //                 <li className="nav-item mx-2">
+        //                     <Link className="nav-link item" to={'/'}>Home </Link>
+        //                 </li>
+        //                 <li className="nav-item mx-2">
+        //                     <Link className="nav-link" to={'/about'}>About</Link>
+        //                 </li>
+        //                 <li className="nav-item mx-2">
+        //                     <Link className="nav-link" to={'/products'}>Products</Link>
+        //                 </li>
+        //                 <li className="nav-item mx-2">
+        //                     <Link className="nav-link" to={'/blog'}>Blog</Link>
+        //                 </li>
+        //                 <li className="nav-item mx-2">
+        //                     <Link className="nav-link" to={'/contact'}>Contact</Link>
+        //                 </li>
+
+        //                 <form className="fs-4">
+        //                     <div>
+        //                         <FaShoppingCart className='mx-4 mt-2 icon' />
+        //                         <FaSearch className='mt-2 icon' />
+        //                     </div>
+        //                 </form>
+
+        //             </ul>
+        //         </div>
+        //     </nav>
+        // </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
 
                 <div className="navbar-brand">
                     <img width="200px" src={imgConfig?.logo1} alt="#" className='logo' />
                 </div>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-
-                    <ul className="navbar-nav fs-5 fw-bold">
-                        <li className="nav-item mx-2">
-                            <Link className="nav-link item" to={'/'}>Home </Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <Link className="nav-link" to={'/about'}>About</Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <Link className="nav-link" to={'/products'}>Products</Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <Link className="nav-link" to={'/blog'}>Blog</Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <Link className="nav-link" to={'/contact'}>Contact</Link>
-                        </li>
-
-                        <form className="fs-4">
-                            <div>
-                                <FaShoppingCart className='mx-4 mt-2 icon' />
-                                <FaSearch className='mt-2 icon' />
-                            </div>
-                        </form>
-
-                    </ul>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div class="navbar-nav fs-5 fw-bold ">
+                        <Link className="nav-link item" to={'/'}>Home </Link>
+                        <Link className="nav-link" to={'/about'}>About</Link>
+                        <Link className="nav-link" to={'/products'}>Products</Link>
+                        <Link className="nav-link" to={'/blog'}>Blog</Link>
+                        <Link className="nav-link" to={'/contact'}>Contact</Link>
+                    </div>
+                    <div className='fs-4'>
+                        <FaShoppingCart className='mx-4 icon' />
+                        <FaSearch className='icon' />
+                    </div>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     )
 }
 
